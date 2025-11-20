aaa885c271687c92642d6b0e1e18975740867401
ahmadansari <gyratedigital@gmail.com>
2025-11-20 10:56:12 +0500

    first commit

---
+++ components/EventCard.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  image: string;
+  title: string;
+  slug: string;
+  location: string;
+  date: string;
+  time: string;
+}
+const EventCard = ({image, title, slug, location, date, time}: Props) => {
+  return (
+    <Link href={`/events/${slug}`} id="event-card">
+        <Image src={image} alt={title} width={410} height={300} className="poster rounded-lg mb-2" />
+        <div className="flex flex-row gap-2">
+          <Image src="/icons/pin.svg" alt="location" width={14} height={14} />
+          <p>{location}</p>
+        </div>
+        <p className="title">{title}</p>
+        <div className="datetime">
+          <div>
+            <Image src="/icons/calendar.svg" alt="date" width={14} height={14} />
+            <p>{date}</p>
+          </div>
+          <div>
+            <Image src="/icons/clock.svg" alt="time" width={14} height={14} />
+            <p>{time}</p>
+          </div>
+        </div>
+        {/* <p className="date">{date}</p>
+        <p className="time">{time}</p> */}
+    </Link>
+  )
+}
+
+export default EventCard
---
+++ lib/constants.ts
@@ -0,0 +1,59 @@
+export type EventItem = {
+    image: string;
+    title: string;
+    slug: string;
+    location: string;
+    date: string;
+    time: string;
+}
+
+export const events: EventItem[] = [
+    {
+        image: "/images/event1.png",
+        title: "React post 1",
+        slug: "react-post1",
+        location: "San Francisco, CA, USA",
+        date: "2025-11-19",
+        time: "07:55 PM",
+    },
+    {
+        image: "/images/event2.png",
+        title: "React post 2",
+        slug: "react-post2",
+        location: "San Lahore, LA, PAK",
+        date: "2025-11-19",
+        time: "07:55 PM",
+    },
+    {
+        image: "/images/event3.png",
+        title: "React post 3",
+        slug: "react-post3",
+        location: "San Lahore, LA, PAK",
+        date: "2025-11-19",
+        time: "07:55 PM",
+    },
+    {
+        image: "/images/event4.png",
+        title: "React post 4",
+        slug: "react-post4",
+        location: "San Lahore, LA, PAK",
+        date: "2025-11-19",
+        time: "07:55 PM",
+    },
+    {
+        image: "/images/event5.png",
+        title: "React post 5",
+        slug: "react-post5",
+        location: "San Lahore, LA, PAK",
+        date: "2025-11-19",
+        time: "07:55 PM",
+    },
+    {
+        image: "/images/event6.png",
+        title: "React post 6",
+        slug: "react-post6",
+        location: "San Lahore, LA, PAK",
+        date: "2025-11-19",
+        time: "07:55 PM",
+    },
+]
